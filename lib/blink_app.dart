36942aedb73a931f42a0670b228511dc5eed21a4
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-03-14 16:21:01 +0200

    create development branch

---
+++ lib/blink_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BlinkApp extends StatelessWidget {
+  const BlinkApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
