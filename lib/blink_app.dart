6d2c03056c236f85b8187ce1829a2d386163b9f6
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-03-14 17:05:35 +0200

    create extension for navigation

--- lib/blink_app.dart
+++ lib/blink_app.dart
@@ -1,4 +1,5 @@
 import 'package:blink2/core/routing/app_router.dart';
+import 'package:blink2/core/routing/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -8,13 +9,18 @@ class BlinkApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return ScreenUtilInit(
-      designSize: const Size(375, 812),
+      designSize: const Size(430, 932),
       minTextAdapt: true,
       child: MaterialApp(
-       title: 'Blink',
+        title: 'Blink',
         debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+        ),
         onGenerateRoute: appRouter.generateRoute,
+        initialRoute: Routes.loginScreen,
       ),
     );
   }
