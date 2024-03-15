b20a67245221784b85c1b8f92f076396f9f7cdaf
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-03-15 16:29:54 +0200

    add app colors file

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColorS {
+  static const Color kOrangeColor = Color(0xffFF9B00);
+  static const Color kOrangeAccentColor = Color(0xffFCB341);
+  static const Color kGrayColor = Color(0xff333333);
+}
