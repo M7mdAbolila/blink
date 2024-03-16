7ad66039644ae6196fbf191215cfa95288704f5d
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-03-16 21:12:18 +0200

    create custom text field

---
+++ lib/core/widgets/cutom_text_field.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:gradient_borders/input_borders/gradient_outline_input_border.dart';
+
+class CustomTextField extends StatefulWidget {
+  const CustomTextField({
+    super.key,
+    required this.isPassword,
+    required this.labelText,
+    required this.contorller,
+  });
+  final String labelText;
+  final bool isPassword;
+  final TextEditingController contorller;
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool passwordVisible = false;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.contorller,
+      obscureText: passwordVisible,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 25,
+          vertical: 0,
+        ),
+        labelText: widget.labelText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        focusedBorder: GradientOutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          gradient: const LinearGradient(
+            colors: [
+              Color(0xffCD55FB),
+              Color(0xffFF4D51),
+              Color(0xffFCB341),
+              Color(0xffFF4D51),
+              Color(0xffCD55FB),
+            ],
+          ),
+        ),
+        suffixIcon: IconButton(
+          onPressed: () {
+            setState(() {
+              passwordVisible = !passwordVisible;
+            });
+          },
+          icon: widget.isPassword
+              ? passwordVisible
+                  ? Image.asset('assets/images/eye_off.png')
+                  : Image.asset('assets/images/eye.png')
+              : const SizedBox(),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/login/ui/widgets/login_screen_body.dart
+++ lib/features/login/ui/widgets/login_screen_body.dart
@@ -2,28 +2,36 @@ import 'package:blink2/core/helpers/spacing.dart';
 import 'package:blink2/core/theme/colors.dart';
 import 'package:blink2/core/widgets/app_logo.dart';
 import 'package:blink2/core/widgets/custom_text_auth.dart';
+import 'package:blink2/core/widgets/cutom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreenBody extends StatelessWidget {
-  const LoginScreenBody({super.key});
-
+   LoginScreenBody({super.key});
+  final email = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const AppLogo(),
-          verticalSpace(103),
-          const CustomTextAuth(
-            text: 'SIGN IN',
-            color: AppColorS.kGrayColor,
-            size: 36,
-            fontWeight: FontWeight.w500,
-          ),
-          verticalSpace(55),
-          
-        ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 51),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const AppLogo(),
+            verticalSpace(103),
+            const CustomTextAuth(
+              text: 'SIGN IN',
+              color: AppColorS.kGrayColor,
+              size: 36,
+              fontWeight: FontWeight.w500,
+            ),
+            verticalSpace(55),
+             CustomTextField(
+              labelText: 'email',
+              contorller: email,
+              isPassword: false,
+            ),
+          ],
+        ),
       ),
     );
   }
