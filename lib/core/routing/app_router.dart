34c9ce340efae5eda6a04a2fc3e3623c0e9efcae
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-03-20 15:46:50 +0200

    fake commit

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -3,7 +3,6 @@ import 'package:blink2/features/home/ui/home_screen.dart';
 import 'package:blink2/features/login/logic/login_cubit/login_cubit.dart';
 import 'package:blink2/features/login/ui/login_screen.dart';
 import 'package:blink2/features/sign_up/ui/sign_up_screen.dart';
-import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
