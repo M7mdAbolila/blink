c2bcac955581f0474a72ae0fb9aa08ab3ae83240
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-03-21 16:25:52 +0200

    add get it and handle Di in feature cubit

--- lib/features/login/logic/login_cubit/login_cubit.dart
+++ lib/features/login/logic/login_cubit/login_cubit.dart
@@ -1,7 +1,6 @@
-import 'package:blink2/core/networking/api_service.dart';
+import 'package:blink2/core/di/service_locator.dart';
 import 'package:blink2/features/login/data/repos/login_repo.dart';
 import 'package:bloc/bloc.dart';
-import 'package:dio/dio.dart';
 import 'package:meta/meta.dart';
 part 'login_state.dart';
 
@@ -13,10 +12,10 @@ class LoginCubit extends Cubit<LoginState> {
     required String password,
   }) async {
     emit(LoginLoading());
-    var result = await LoginRepo(ApiService(Dio())).login(
-      email: email,
-      password: password,
-    );
+    var result = await getIt.get<LoginRepo>().login(
+          email: email,
+          password: password,
+        );
 
     result.fold(
       (failure) => emit(
