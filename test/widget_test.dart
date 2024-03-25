3d8da716f51ff7b92f0df7ec1bda510af8f9a868
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-03-25 23:45:51 +0200

    finish sprint one

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,6 @@
 
 import 'package:blink2/blink_app.dart';
 import 'package:blink2/core/routing/app_router.dart';
-import 'package:blink2/blink_app.dart';
-import 'package:blink2/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
